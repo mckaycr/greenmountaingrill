2a56c0e08d30b7e370c1c7a9179b071907869227
Christopher McKay <mckaycr@gmail.com>
2023-01-27 11:01:24 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+   name="gmg",
+   version="0.0.1",
+   description="Middleware for talking to your Green Mountain Grill",
+   long_description=README,
+   long_description_content_type="text/markdown",
+   author="Christopher McKay",
+   author_email="mckaycr@gmail.com",
+   license="MIT",
+   packages=["gmg"],
+   zip_safe=False
+)
