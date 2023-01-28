be03b7d4540cd3903a3a35f6d5e26743cd28cb0b
Christopher McKay <mckaycr@gmail.com>
2023-01-27 19:43:30 -0500

    bumping up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 setup(
    name="gmg",
-   version="0.0.1",
+   version="0.0.3",
    description="Middleware for talking to your Green Mountain Grill",
    long_description=README,
    long_description_content_type="text/markdown",
